41dcfe052811a368a6cd0bfbc6de9332b7516d7b
Santos220 <bianca.santos@simoespires.com>
2026-04-16 13:21:20 -0300

    refactor: isolate draft method

--- desafio2_BD/player_hand.py
+++ desafio2_BD/player_hand.py
@@ -1,37 +1,24 @@
 import tournament_core
+import interface.interface as Interface
 
-# Função de Menu
-def obter_escolha_segura(lista_opcoes, mensagem_cabecalho):
-    print(f"\n{mensagem_cabecalho}")
-    for i, opcao in enumerate(lista_opcoes):
-        nome = opcao.name if hasattr(opcao, 'name') else opcao
-        print(f"[{i + 1}] {nome}")
-        
-    while True:
-        try:
-            escolha = int(input(f"Digite o número (1-{len(lista_opcoes)}): ")) - 1
-            if 0 <= escolha < len(lista_opcoes):
-                return lista_opcoes.pop(escolha)
-            print("Número inválido! Escolha uma das opções acima.")
-        except ValueError:
-            print("Por favor, digite apenas números.")
-
-
-# Escolha das Cartas (Draft)
 def realizar_draft(player1, player2, avaiable_algorithms):    
     while len(player1.hand) < 4 or len(player2.hand) < 4:    
         moeda = tournament_core.GameEngine.coin_toss()
+        
         if moeda == 'Player 1' and len(player1.hand) < 4:
-            carta = obter_escolha_segura(avaiable_algorithms, f">>> DRAFT: {player1.name}, escolha uma carta:")
-            player1.hand.append(carta)     
+            Interface.MatchMessages.draft_escolha_carta(player1)
+            idx = Interface.DataInterface.menu_escolha_segura(avaiable_algorithms)
+            carta = avaiable_algorithms.pop(idx)
+            player1.hand.append(carta)
+                 
         elif moeda == 'Player 2' and len(player2.hand) < 4:
-            carta = obter_escolha_segura(avaiable_algorithms, f">>> DRAFT: {player2.name}, escolha uma carta:")
+            Interface.MatchMessages.draft_escolha_carta(player2)
+            idx = Interface.DataInterface.menu_escolha_segura(avaiable_algorithms)
+            carta = avaiable_algorithms.pop(idx)
             player2.hand.append(carta) 
             
-    # Tratamento de objetos após o Draft
     player1.hand = tournament_core.criar_algoritmos(player1.hand)
     player2.hand = tournament_core.criar_algoritmos(player2.hand)
     
-    print("\n--- Estratégias escolhidas: ---")
-    print(f"{player1.name}: {[c.name for c in player1.hand]}")
-    print(f"{player2.name}: {[c.name for c in player2.hand]}\n")
+    Interface.SystemMessages.mostrar_estrategias(player1, player2)
+    
