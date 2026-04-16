f006713b409c3152504daf59aa0f0f0e290da1f8
Santos220 <bianca.santos@simoespires.com>
2026-04-16 13:20:37 -0300

    refactor: remove prints, adding interface methods

--- desafio2_BD/manager.py
+++ desafio2_BD/manager.py
@@ -1,6 +1,7 @@
 import PrisonersDilemma
 import database
-from player_hand import obter_escolha_segura
+import interface.interface as Interface
+
 
 class Juiz:
     def __init__(self, p1, p2, session, partida_db):
@@ -8,28 +9,20 @@ class Juiz:
         self.p2 = p2
         self.session = session
         self.partida_db = partida_db
-    
-    def anunciar_vencedor_turno(self, pontos1, pontos2, alg1, alg2):
-        if pontos1 > pontos2:
-            print(f"\n-> Vitória de {self.p1.name}! ({alg1.name} - {pontos1} x {pontos2} - {alg2.name})")
-        elif pontos2 > pontos1:
-            print(f"\n-> Vitória de {self.p2.name}! ({alg2.name} - {pontos2} x {pontos1} - {alg1.name})")
 
-    def anunciar_empate(self, pontos1, alg1, alg2):
-        print(f"\n-> Empate na série: {pontos1} pontos cada. | {alg1.name} vs {alg2.name}")
+    def devolver_cartas(self, alg1, alg2):
         self.p1.hand.append(alg1)
         self.p2.hand.append(alg2)
 
     def declarar_vencedor_final(self, vitorias1):
-        print("\n" + "-"*30)
         if vitorias1 == 3:
             vencedor, id_vencedor = self.p1.name, self.p1.id_db
         else:
             vencedor, id_vencedor = self.p2.name, self.p2.id_db
-            
-        print(f"🏆 VENCEDOR: {vencedor.upper()}!")
+        
+        Interface.MatchMessages.mostrar_vencedor_final(vencedor)
         database.registrar_vencedor(self.session, self.partida_db, id_vencedor)
-        print("-" * 30)
+        
 
 
 class Pontuador:
@@ -71,16 +64,30 @@ class TorneioManager:
         self.juiz = Juiz(p1, p2, session, partida_db)
     
     def executar_torneio(self):
-        print("\n" + "="*30 + "\n   INÍCIO DO TORNEIO!\n" + "="*30)
+        Interface.SystemMessages.torneio_inicio()
         vitorias1, vitorias2 = 0, 0
         
         while vitorias1 < 3 and vitorias2 < 3:
             if vitorias1 == 2 and vitorias2 == 2:
-                alg1 = obter_escolha_segura(self.p1.discard_pile, f"[!] EMPATE 2x2! TUDO OU NADA!\n>>> {self.p1.name}, resgate do descarte:")
-                alg2 = obter_escolha_segura(self.p2.discard_pile, f"[!] EMPATE 2x2! TUDO OU NADA!\n>>> {self.p2.name}, resgate do descarte:")
+                # Player 1
+                Interface.MatchMessages.resgaste_descarte(self.p1)
+                escolha1 = Interface.DataInterface.menu_escolha_segura(self.p1.discard_pile)
+                alg1 = self.p1.discard_pile.pop(escolha1)
+                # Player 2
+                Interface.MatchMessages.resgaste_descarte(self.p2)
+                escolha2 = Interface.DataInterface.menu_escolha_segura(self.p1.discard_pile)
+                alg2 = self.p1.discard_pile.pop(escolha2)
+                
             else:
-                alg1 = obter_escolha_segura(self.p1.hand, f">>> BATALHA: {self.p1.name}, escolha sua carta:")
-                alg2 = obter_escolha_segura(self.p2.hand, f">>> BATALHA: {self.p2.name}, escolha sua carta:")
+                # Player 1
+                Interface.MatchMessages.escolha_carta_batalha(self.p1)
+                escolha1 = Interface.DataInterface.menu_escolha_segura(self.p1.hand)
+                alg1 = self.p1.hand.pop(escolha1)
+                # Player 2
+                Interface.MatchMessages.escolha_carta_batalha(self.p2)
+                escolha2 = Interface.DataInterface.menu_escolha_segura(self.p2.hand)
+                alg2 = self.p2.hand.pop(escolha2)
+                
             
             combate = PrisonersDilemma.Combate(alg1, alg2, self.ruido, self.num_rodadas)
             resultado = combate.jogar()
@@ -90,12 +97,13 @@ class TorneioManager:
             vitorias1, vitorias2, status = Pontuador.atualizar_vitorias(pontos1, vitorias1, pontos2, vitorias2)
             
             if status == 0:
-                self.juiz.anunciar_empate(pontos1, alg1, alg2)
+                self.juiz.devolver_cartas(pontos1, alg1, alg2)
+                Interface.MatchMessages.empate_rodada(pontos1, alg1, alg2)
             else:
-                self.juiz.anunciar_vencedor_turno(pontos1, pontos2, alg1, alg2)
+                Interface.MatchMessages.resultado_rodada(self.p1, self.p2, pontos1, pontos2, alg1, alg2)
             
             self.p1.discard_pile.append(alg1)
             self.p2.discard_pile.append(alg2)
-            print(f"Placar Atual: {self.p1.name} {vitorias1} x {vitorias2} {self.p2.name}\n")
+            Interface.MatchMessages.placar_atual(self.p1, self.p2, vitorias1, vitorias2)
 
         self.juiz.declarar_vencedor_final(vitorias1)
