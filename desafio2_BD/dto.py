256092fffbab93dd11b83dbc582804e6b52664be
Santos220 <bianca.santos@simoespires.com>
2026-04-22 14:41:50 -0300

    feat: add matches dto

--- desafio2_BD/dto.py
+++ desafio2_BD/dto.py
@@ -1,14 +1,29 @@
 from dataclasses import dataclass
-from typing import List
-
-# Imutáveis e Pequenos
 
 @dataclass
 class CadastroJogadoresDTO:
     nome_j1: str
     nome_j2: str
 
-@dataclass
-class EscolhaCarta:
-    nome_jogador: str
-    cartas: List[str]
+@dataclass(frozen=True)
+class PartidaConfigDTO:
+    id_partida: int
+    ruido: float
+    num_rodadas: int
+
+@dataclass(frozen=True)
+class AcaoRodadaDTO:
+    num_rodada: int
+    j1_escolha_real: str
+    j1_escolha_pos_ruido: str
+    j1_pontos: int
+    j2_escolha_real: str
+    j2_escolha_pos_ruido: str
+    j2_pontos: int
+
+@dataclass(frozen=True)
+class ResumoTurnoDTO:
+    num_serie: int
+    id_algoritmo_j1: int
+    id_algoritmo_j2: int
+    vencedor_turno: int
